d524906f8caa8ff9e98449ca2049c8258ff2b389
NobiSamid <nobisamid@gmail.com>
2025-11-08 22:26:52 +0600

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,37 @@
+import { Spotlight } from './ui/Spotlight'
+import { cn } from '@/utils/cn'
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+       <div className="relative flex h-[50rem] w-full items-center justify-center bg-white dark:bg-black">
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:20px_20px]",
+          "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
+          "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
+        )}
+      />
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+      <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
+        Backgrounds
+      </p>
+    </div>
+    </div>
+  )
+}
+
+export default Hero
